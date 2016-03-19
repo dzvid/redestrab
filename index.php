92627a190983d59997ecb723631ef18a98348bd2
thehiddenbuddy <oliveiradavid007@gmail.com>
2016-03-18 21:27:16 -0400

    alterei o codigo
    Fiz do "jeito do professor"

--- index.php
+++ index.php
@@ -4,37 +4,23 @@
         <title>Gerenciador de redes</title>
     </head>
     <body>
-        <h1>Gerenciador de rede Tabajara</h1>
+       <table width=580 border=0 cellspacing=0 cellpadding=0 bgcolor="#ffffff" align=center>
+        <tr><td><font face="Comic SansMSa"><font color="black"><h1 align=center>Gerenciador de rede Tabajara</h1></td></tr>
         <form action="processa_consulta_elemento_rede.php" method="post">
-        <table width=580 border=0 cellspadding=0 cellpading=0 bgcolor="#FFFFFF" align=center>
-        
+            <table width=680 border=0 cellspacing=2 cellpadding=0 bgColor="#ffffff" align=center>
+                <br>
+         <td><font face="Comic SansMSa"><font color="black"><h3>Ip do elemento de rede:</h3>
+         <td width="546"><input id="ip" maxlength="30" name="ip" size="60" type="text"></td>
+         <br>
+         <br>
          <tr>
-             <td>
-                 <font face="Arial" color="black">
-                     <h3>Ip do elemento de rede:</h3>
-                     <td width="546"><input type="text" name="ip" size="60" maxlength="30"></td>
-                     <br>
-                     <br>
-         </tr>
-        <tr>
-            <td> 
-            <font face="Arial" color="black">
-                     <h3>Object ID: (Info. gerenciada)</h3>
-                     <td width="546"><input type="text" name="id" size="60" maxlength="30"></td></td>
-        </tr>
-        <tr>
-            <td align=center colspan=3>
-                <input id="pesquisar" type="submit" value="Pesquisar">
-                <input type="reset" value="Limpar">
-                
-            </td>
-        </tr>
-        <tr>
-            <td align=center colspan=3>
-                
-            </tbody>
-        </tr>
-        </table>
-        </form>
-    </body>
-</html>
+             <td><font face="Comic SansMSa"><font color="black"><h3>Object ID: (Informação gerenciada)</h3></td>
+             <td width="546"><input id="id" maxlength="30" name="id" size="60" type="text"></td>
+             </tr>
+        <tr><td align=center colspan=3>
+            <input id="pesquisar" type=submit value="Pesquisar">
+            <input type=reset value="Limpar"></td></tr>
+        <tr><td align=center colspan=3>
+            </tbody></table>
+            </form>
+            </html>
--- processa_consulta_elemento_rede.php
+++ processa_consulta_elemento_rede.php
@@ -1,16 +1,19 @@
 <?php
-$ip=$_POST['ip'];
-$oid=$_POST['id'];
-    $a=snmpwalkoid("$ip","public","$oid");
-$result = array();
-echo '<table align=center width=800 cellspacing=2 border=2>';
-echo '<tr><td bgcolor=#FFFFFF width=50>$ip</td></tr>';
-echo '<tr><td bgcolor=#FFFFFF width=20>Parametro</td><td bgcolor=#FFFFFF width=30>Nome</td></tr>';
-
-    $i=0;
-    foreach($a as $i => $n)
-{
-    $result[$n] = $a[$i];
-    echo '<tr><td bgcolor=#FFFFFF width=30>$i</td><td bgcolor=#FFFFFF width=60>$n</td></tr>';
-}
-?>
+	//snmp_read_mib('C:\xampp\php\extras\mibs\AGENTX-MIB.txt');
+	//snmprealwalk('localhost','public','AGENTX-MIB::" "');
+	
+	$ip=$_POST['ip'];
+	$oid=$_POST['id'];
+	$a = snmpwalkoid("$ip","public","$oid");
+	$result = array();
+	echo "<table align=center widht=800 cellspacing=2 border=2>";
+    echo "<tr><td bgColor=#FFFFFF widht=50>Host: $ip</td>";
+	//echo "<table  align=center widht=5 cellspacing=2 border=2>";
+	echo "<tr><td bgColor=#FFFFFF widht=20>Parametros</td><td bgColor=#FFFFFF widht=30>Nome</td></tr>";
+	$i=0;
+		foreach($a as $i => $n){
+			$result[$n] = $a[$i];
+			echo "<tr><td bgColor=#FFFFFF widht=30>$i</td><td bgColor=#FFFFFF widht=60>$n</td></tr>";			
+		}
+		//print_r($result);
+	?>
