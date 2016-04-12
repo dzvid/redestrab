7cb21f538ccf19d289ffa85b08ef3c226b4fb1af
thehiddenbuddy <oliveiradavid007@gmail.com>
2016-04-12 14:29:50 -0400

    Alterei o nome do gerenciador
    Tabajara para Skynet e eliminei o botão "Limpar".

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
     </head>
     <body>
        <table width=580 border=0 cellspacing=0 cellpadding=0 bgcolor="#ffffff" align=center>
-        <tr><td><font face="Comic SansMSa"><font color="black"><h1 align=center>Gerenciador de rede Tabajara</h1></td></tr>
+        <tr><td><font face="Comic SansMSa"><font color="black"><h1 align=center>Gerenciador de rede Skynet</h1></td></tr>
         <form action="processa_consulta_elemento_rede.php" method="post">
             <table width=680 border=0 cellspacing=2 cellpadding=0 bgColor="#ffffff" align=center>
                 <br>
-         <td><font face="Comic SansMSa"><font color="black"><h3>Ip do elemento de rede:</h3>
+         <td><font face="Comic SansMSa"><font color="black"><h3>Endereço ip do dispositivo:</h3>
          <td width="546"><input id="ip" maxlength="30" name="ip" size="60" type="text"></td>
          <br>
          <br>
         <tr><td align=center colspan=3>
             <input id="pesquisar" type=submit value="Pesquisar">
-            <input type=reset value="Limpar"></td></tr>
+            
         <tr><td align=center colspan=3>
             </tbody></table>
             </form>
